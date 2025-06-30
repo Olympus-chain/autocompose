52bdc69cc660cac51fa4cc71ba9ddc677c61ec1c
Romain Leonard <romain.leonard@olympus-chain.fr>
2025-06-30 11:06:25 +0200

    Update cli.rs version

--- src/cli.rs
+++ src/cli.rs
@@ -17,7 +17,7 @@ use std::path::PathBuf;
 #[command(
     name = "autocompose",
     about = "Generate Docker Compose files from running containers",
-    version = "2.0.0"
+    version = "1.5.0"
 )]
 pub struct Cli {
     #[command(subcommand)]
